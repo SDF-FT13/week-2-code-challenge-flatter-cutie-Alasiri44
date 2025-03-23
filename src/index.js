f772884f2d8166f4d6056937adfab5ae1ee7c3e2
alasiri <austinalasiri44@gmail.com>
2025-03-23 16:25:03 +0300

    Finished the deliverables

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const changingImage = document.querySelector('.characterInfo img');
 const totalVotes = document.querySelector('#vote-count');
 const form = document.getElementById('votes-form');
 const input = document.getElementById('votes');
+const resetButton = document.getElementById('reset-btn');
 let myData = 0;
 fetch('http://localhost:3000/characters')
 .then(res => res.json())
@@ -16,11 +17,11 @@ fetch('http://localhost:3000/characters')
 
 function addNames(newData){
     for(let element of newData){
-        const newCharacter = document.createElement('div');
+        const newCharacter = document.createElement('span');
         newCharacter.classList.add('names-of-images')
         newCharacter.style.width = '50px';
         
-        newCharacter.style.padding = '22px 8px'
+        newCharacter.style.padding = '11px 13px'
         newCharacter.style.border = '1px solid white'
         newCharacter.style.borderRadius = '60px';
         newCharacter.textContent = element.name;
@@ -41,31 +42,64 @@ function addEventListener(newData){
             totalVotes.textContent = finder.votes;
 
             addVotes(finder);
+            removeVotes(finder);
         }      
     })
 }
 
 function addVotes(finder){
-    form.addEventListener('submit', function(e){
-        e.preventDefault();
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
 
        if (parseInt(input.value, 10) > 0) {
          finder.votes +=   parseInt(input.value, 10);
          console.log(typeof(parseInt(input.value, 10)));
          
          totalVotes.textContent = finder.votes;
-
-         fetch('http://localhost:3000/characters', {
-            method: 'POST',
+           
+         fetch(`http://localhost:3000/characters/${finder.id}`,{
+            method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
+                'Accept': 'application/json'                
+            },
+            body: JSON.stringify({
+                votes: finder.votes
+            })
          })
          .then(res => res.json())
-         .then(data => console.log(data)
-         )
+         .then(data => console.log(data))
+         .catch(error => {
+            console.log(error);
+         })
        }
     })
 }
+
+function removeVotes(finder){
+    resetButton.addEventListener('click', function(event){
+        event.preventDefault();       
+         
+         totalVotes.textContent = 0;
+           
+         fetch(`http://localhost:3000/characters/${finder.id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json'               
+            },
+            body: JSON.stringify({
+                votes: 0
+            })
+         })
+         .then(res => res.json())
+         .then(data => console.log(data))
+         .catch(error => {
+            console.log(error);
+         })
+       }
+    )
+}
+
+
 
